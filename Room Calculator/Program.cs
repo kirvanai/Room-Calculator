be689b093e242f14c23982e963a2f85259ba6315
kirvanai <kirvanirving@gmail.com>
2023-10-02 14:04:33 -0400

    Added Room Size

--- Room Calculator/Program.cs
+++ Room Calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 public class Program
 {
@@ -8,7 +9,7 @@ public class Program
         Console.WriteLine("Enter Length:");
         string entryLength = Console.ReadLine();
         double length = double.Parse(entryLength);
-        Console.WriteLine("Enter Width:");
+        Console.WriteLine("Enter Width:.");
         string entryWidth = Console.ReadLine();
         double width = double.Parse(entryWidth);
         Console.WriteLine("Enter Height:");
@@ -18,10 +19,28 @@ public class Program
         double perimeter = width + width + length + length;
         double volume = length * width * height;
         double surfaceArea = 2 * (area) + 2 * (length * height) + 2 * (width * height);
+        string roomClassification;
+        
+        if (area <= 250)
+            {
+            roomClassification = "small";
+            }
+        else if (area > 250 & area < 650) 
+            {
+            roomClassification = "medium";
+            }
+        else 
+            {
+            roomClassification = "large";
+            }
+        
+        
+        
         Console.WriteLine($"Area: {area}");
         Console.WriteLine($"Perimeter: {perimeter}");
         Console.WriteLine($"Volume: {volume}");
         Console.WriteLine($"Surface Area: {surfaceArea}");
+        Console.WriteLine($"Room Size: {roomClassification}");
         Console.WriteLine("Thank you for using the Room Detail Generator!");
     }
 }
